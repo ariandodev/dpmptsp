850bedcd42f7c1a66ebd3a195d809890c01430b5
ariandodev <ariandomtuwe@gmail.com>
2025-02-17 14:27:23 +0700

    Menambahkan seeder data user dan permission

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,8 @@ namespace Database\Seeders;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
@@ -16,8 +18,40 @@ class DatabaseSeeder extends Seeder
         // User::factory(10)->create();
 
         User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+            'name' => 'Admin',
+            'email' => 'admin@admin.com',
+            'password' => Hash::make('12345678'),
+            'role_id' => 1,
+        ]);
+
+        DB::table('permissions')->insert([
+            'permission' => 'dashboard',
+        ]);
+
+        DB::table('permissions')->insert([
+            'permission' => 'skm',
+        ]);
+        DB::table('permissions')->insert([
+            'permission' => 'hasil_skm',
+        ]);
+        DB::table('permissions')->insert([
+            'permission' => 'kelola_data_layanan',
+        ]);
+        DB::table('permissions')->insert([
+            'permission' => 'kelola_data_pertanyaan',
+        ]);
+        DB::table('permissions')->insert([
+            'permission' => 'simulasi',
+        ]);
+
+        DB::table('permissions')->insert([
+            'permission' => 'data_pengguna_dan_hak_akses',
+        ]);
+        DB::table('permissions')->insert([
+            'permission' => 'data_pengguna',
+        ]);
+        DB::table('permissions')->insert([
+            'permission' => 'kelola_hak_akses',
         ]);
     }
 }
